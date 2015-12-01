7e52d608136138d1302f4fdb25014ab4d91b1eb1
Andy Garcia <garciaan@oregonstate.edu>
2015-12-01 12:08:28 -0800

    Add mentees button now works. It also prevents from adding the same mentee multiple times. NOTE: TABLE ALTERED (mentor2mentee):: ADDED UNIQUE COMBO OF IDS

--- wp-content/themes/zerif-lite/mentee_add.php
+++ wp-content/themes/zerif-lite/mentee_add.php
@@ -29,41 +29,78 @@ get_header(); ?>
 				
 				<?php
 					global $wpdb;
-					$results = $wpdb->get_results("SELECT * FROM mentee JOIN mentor_career ON mentee.mentee_id = mentor_career.mentee_ID JOIN career_type ON career_type.Career_id = mentor_career.career_ID GROUP By mentee.mentee_id");
 
 					$user_id = get_current_user_id();
 					$sql = 'select `mentor_id` from wpid_to_mid where `wp_id`= ' . $user_id;
 					$mentor_id = (int)($wpdb->get_var($sql));
 
-					echo "<table border=0>";
+					if(isset($_POST['add_mentee'])) {
+						$mentee_id = $_POST['add_mentee'];
+						//echo "<h1>Mentee ID Chosen: " . $mentee_id . "</h1>";
+						//$sql = "SELECT * FROM mentee JOIN mentor_career ON mentee.mentee_id = mentor_career.mentee_ID JOIN career_type ON career_type.Career_id = mentor_career.career_ID WHERE mentee.mentee_id = '" . $mentee_id . "'";
+						$sql = "INSERT INTO mentor2mentee (`mentor_id`,`mentee_id`) VALUES ('" . $mentor_id . "','" . $mentee_id . "') ON DUPLICATE KEY UPDATE `mentor_id` = `mentor_id`;";
+						//echo $sql; 
+						$wpdb->get_results($sql);
 
-					if(!empty($results)) { 
-     					foreach($results as $r) {	 
-          					echo "<tr><td>";
-          					echo "<table border=0>";
-          					echo "<tr>";
-          					echo "<td rowspan=2 width=200><img class= wp-image-34 size-thumbnail src=" .$r->photo. " width= 150 /></td>";
-          					echo "<td><h1>".$r->full_name."</h1><h2>".$r->employer."</h2></td>";
-          					echo "</tr>";
-          					echo "<tr><td><h3>Expertise</h3>:" .$r->Career_Name. "</td></tr>";
-          					echo "<tr><td></td><td>Years Experience: ".$r->yrs_exp."</td></tr>";
-          					echo "<tr><td></td><td>".$r->desc_exp."</td></tr>";
-          					echo "<tr><td></td><td>Address: ".$r->address." ".$r->State.", ".$r->Country."</td></tr>";
-          					echo "<tr><td></td><td>Contact Me:</td></tr>";
-          					echo "<tr><td></td><td>Preferred Method: ".$r->contact_meth."</td></tr>";
-          					echo "<tr><td></td><td>Email: ".$r->email."</td></tr>";
-          					echo "<tr><td></td><td>Phone: ".$r->phone."</td></tr>";
-          					echo "<tr><td></td><td>".$r->session_num." Sessions of ".$r->session_time." mins per year requested</td></tr>";
-          					echo "<tr><td></td><td>References: ".$r->ref_1."<br/>References: ".$r->ref_2."</td></tr>";
-          					echo "<tr><td></td><td>Why I am a Mentee: ".$r->why_mentee."</td></tr>";
-          					echo "<tr><td></td><td><form method=post id=menteeadd_form><input type=hidden name=menteeID id=menteeID value=".$r->mentee_id."><input type=submit value='Add Mentee'></form></td></tr>";
-          					echo "</table>";
-          					echo "</td></tr>";
-     					}
-					} else {
-     					echo "ERROR: SELECT returned with ".$wpdb->print_error();	 	 
-					} 
-					echo "</table>";
+						echo "<h1>Thank you. You have successfully added a mentee!</h1>";
+
+						//TESTING
+						$test_sql = "SELECT * FROM mentor2mentee WHERE `mentor_id` = '" . $mentor_id . "' AND `mentee_id` = '" . $mentee_id . "'";
+						$result = $wpdb->get_row($test_sql);
+						echo "<hr><hr><hr><p><h1>FOR TESTING PURPOSES ONLY</h1></p><br><br>";
+						echo "<p>ORIGINAL SQL STATEMENT: " . $sql . "</p><br>";
+						echo "<p>TESTING SQL STATEMENT: " . $test_sql . "</p><br>";
+						echo "<p>Input: (" . $mentor_id . "," . $mentee_id . ")";
+						echo "<p>Expected output: (" . $mentor_id . "," . $mentee_id . ")";
+						echo "<p>Acual Output: (" . $result->mentor_id . "," . $result->mentee_id . ")";
+						echo '<table style="width:30%;float:center"><tr><td>Does input equal output?</td>';
+						if ($mentor_id == $result->mentor_id && $mentee_id == $result->mentee_id){
+							echo '<td bgcolor="#008000"> PASS </td>';
+						}
+						else {
+							echo '<td bgcolor="#FF0000"> FAIL </td>';
+						}
+						echo "</tr></table>";
+
+						echo "<hr><hr><hr><br><br>";
+
+						
+						//echo $sql;
+
+					}
+					else {
+						$results = $wpdb->get_results("SELECT * FROM mentee JOIN mentor_career ON mentee.mentee_id = mentor_career.mentee_ID JOIN career_type ON career_type.Career_id = mentor_career.career_ID GROUP By mentee.mentee_id");
+
+						
+						echo "<form method=post id=menteeadd_form>";
+						echo "<table border=0>";
+
+						if(!empty($results)) { 
+	     					foreach($results as $r) {	 
+	     						
+	          					echo "<tr>";
+	          					echo "<td rowspan=2 width=200><img class= wp-image-34 size-thumbnail src=" .$r->photo. " width= 150 /></td>";
+	          					echo "<td><h1>".$r->full_name."</h1><h2>".$r->employer."</h2></td>";
+	          					echo "</tr>";
+	          					echo "<tr><td><h3>Expertise</h3>:" .$r->Career_Name. "</td></tr>";
+	          					echo "<tr><td></td><td>Years Experience: ".$r->yrs_exp."</td></tr>";
+	          					echo "<tr><td></td><td>".$r->desc_exp."</td></tr>";
+	          					echo "<tr><td></td><td>Address: ".$r->address." ".$r->State.", ".$r->Country."</td></tr>";
+	          					echo "<tr><td></td><td>Contact Me:</td></tr>";
+	          					echo "<tr><td></td><td>Preferred Method: ".$r->contact_meth."</td></tr>";
+	          					echo "<tr><td></td><td>Email: ".$r->email."</td></tr>";
+	          					echo "<tr><td></td><td>Phone: ".$r->phone."</td></tr>";
+	          					echo "<tr><td></td><td>" .$r->session_num." Sessions of ".$r->session_time." mins per year requested</td></tr>";
+	          					echo "<tr><td></td><td>" . "References: ".$r->ref_1."<br/>References: ".$r->ref_2."</td></tr>";
+	          					echo "<tr><td></td><td>Why I am a Mentee: ".$r->why_mentee."</td></tr>";
+	          					echo "<tr><td></td><td><button type='submit' value='" . $r->mentee_id . "' name='add_mentee'>Add Mentee</button></td></tr>";
+	     					}
+						} else {
+	     					echo "ERROR: SELECT returned with ".$wpdb->print_error();	 	 
+						} 
+						echo "</table>";
+						echo "</form>";
+					}	
 
 				?>
 
